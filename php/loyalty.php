2bd2e4da3f004bcd7bdb1eb9562a9482dd4d48d6
ccrepez <christa.caitlyn@gmail.com>
2026-06-09 06:19:16 +0700

    Update loyalty.php

--- php/loyalty.php
+++ php/loyalty.php
@@ -14,7 +14,13 @@ if ($aksi === 'redeem') {
     $warna = $conn->real_escape_string($_POST['warna'] ?? '');
 
     $loyalty = $conn->query("SELECT poin FROM loyalty_card WHERE id_user=$id_user")->fetch_assoc();
-    $poin = $loyalty ? (int) $loyalty['poin'] : 0;
+    $poin = 0; 
+
+    if ($loyalty) { 
+        $poin = (int) $loyalty['poin']; 
+    } else {
+        $poin = 0; 
+    }
 
     if ($poin < 10) {
         header("Location: ../profile.php?msg=redeem_fail");
